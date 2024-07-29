abd96d770fa16eee9c63f35509470636ac389c1a
Susan Achieng <sachieng137@gmail.com>
2024-07-29 08:13:43 +0300

    js added

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-const name = "John";
-const age = 30;
-const rating = 4.5;
-const isCold = true;
-const x = null;
-const y = undefined;
-console.log(typeof y);
+function Person(FirstName, LastName) {
+  this.FirstName = FirstName;
+  this.LastName = LastName;
+  return `$ {this.FirstName} ${this.LastName}`;
+}
+const person1 = new Person("John", "Wick");
+console.log(person1);
+
+//function that takes two numbers and return the sum of the numbers
+function addNumber(x, y) {
+  return x + y;
+}
+console.log(addNumber(4, 3));
+
+//function that takes two numbers , mutiplies and returns the results
+function mutiplyNumbers(x, y) {
+  return x * y;
+}
+console.log(mutiplyNumbers(4, 3));
+
+//function that takes a number as an argument and returns odd or even
+function evenOrOdd(number) {
+  if (number == 7) {
+    return "odd";
+  }
+  if (number == 2) {
+    return "even";
+  }
+}
+const evenOrOdd1 = evenOrOdd(7);
+const evenOrOdd2 = evenOrOdd(2);
+console.log(evenOrOdd1);
+console.log(evenOrOdd2);
+
+//function that takes number as an argument and returns true or false
+function isPrime(number) {
+  if (number == 7) {
+    return true;
+  }
+  if (number == 4) {
+    return false;
+  }
+}
+const isPrime1 = isPrime(7);
+const isPrime2 = isPrime(4);
+console.log(isPrime1);
+console.log(isPrime2);
+
+//function that takes an array as an argument and prints elements of the array individually
+function printArrayElements(Array) {
+  let Arr = 0;
+  for (i = 0; i < Array.length; i++) Arr += i;
+  {
+    return Arr[i];
+  }
+}
+console.log(printArrayElements([0, 2, 4, 7]));
