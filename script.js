e74b19116a1374a33ec3a4ff4bd06efe8ad5b024
susanachieng <sachieng137@gmail.com>
2024-10-19 14:13:41 +0300

    rehosting my site

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.getElementById(" deposit button"));
-// var depositButton = document.getElementById(" deposit button");
-// depositButton.style.backgroundColor = blue;
-// var transaction = document.querySelector(".transaction");
-// console.log(transaction);
-// transaction.innerText = "list transaction";
-// transaction.computedStyleMap.color = "blue";
-function isPrime(num) {
-  if (num <= 1) return false;
-  for (let i = 2; i <= num; i++) {
-    if (num % i === 0) return false;
-  }
-  return true;
-}
-
-function hasPrimeNumbers(arr) {
-  let count = 0;
-  for (const num of arr)
-    if (isPrime(num)) {
-      count++;
-    }
-  return count;
-}
-const arr = [0, 2, 4, 7];
-console.log(hasPrimeNumbers(arr));
+// Toggle mobile menu
+document.querySelector(".fa-bars").addEventListener("click", function () {
+  const navMenu = document.querySelector("nav ul");
+  navMenu.classList.toggle("show");
+});
